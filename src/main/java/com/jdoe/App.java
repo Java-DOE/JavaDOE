eaacc5541192a89ebf8b9613af4276a064b0e1e1
nomus <mustafanoor715@gmail.com>
2025-12-10 22:39:53 +0500

    project structure

---
+++ src/main/java/com/jdoe/App.java
@@ -0,0 +1,13 @@
+package com.jdoe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
